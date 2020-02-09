4c627e49cb5d37023e2d842c59e80911733569c9
ilkertacyildiz <tacyildizilker@gmail.com>
2020-02-09 17:24:55 +0300

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelHours = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelMinutes = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelSeconds = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelHours
+            // 
+            this.labelHours.BackColor = System.Drawing.Color.Black;
+            this.labelHours.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelHours.ForeColor = System.Drawing.Color.White;
+            this.labelHours.Location = new System.Drawing.Point(14, 46);
+            this.labelHours.Name = "labelHours";
+            this.labelHours.Size = new System.Drawing.Size(90, 34);
+            this.labelHours.TabIndex = 0;
+            this.labelHours.Text = "00";
+            this.labelHours.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(33, 92);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Enabled = false;
+            this.btnPause.Location = new System.Drawing.Point(114, 92);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.BtnPause_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(195, 92);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(14, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Hours";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(105, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 31);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Minutes";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelMinutes
+            // 
+            this.labelMinutes.BackColor = System.Drawing.Color.Black;
+            this.labelMinutes.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelMinutes.ForeColor = System.Drawing.Color.White;
+            this.labelMinutes.Location = new System.Drawing.Point(105, 46);
+            this.labelMinutes.Name = "labelMinutes";
+            this.labelMinutes.Size = new System.Drawing.Size(90, 34);
+            this.labelMinutes.TabIndex = 7;
+            this.labelMinutes.Text = "00";
+            this.labelMinutes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(196, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 31);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Seconds";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelSeconds
+            // 
+            this.labelSeconds.BackColor = System.Drawing.Color.Black;
+            this.labelSeconds.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelSeconds.ForeColor = System.Drawing.Color.White;
+            this.labelSeconds.Location = new System.Drawing.Point(196, 46);
+            this.labelSeconds.Name = "labelSeconds";
+            this.labelSeconds.Size = new System.Drawing.Size(90, 34);
+            this.labelSeconds.TabIndex = 9;
+            this.labelSeconds.Text = "00";
+            this.labelSeconds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 128);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelSeconds);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelMinutes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.labelHours);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Chronometer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHours;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelMinutes;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelSeconds;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int Hours = 0;
+        int Minutes = 0;
+        int Seconds = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //  Form açılırken çalışacaklar
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Seconds++;
+
+            if (Seconds == 60)
+            {
+                Minutes++;
+                Seconds = 0;
+            }
+
+            if (Minutes == 60)
+            {
+                Hours++;
+                Minutes = 0;
+            }
+
+            if (Seconds < 10)
+            {
+                labelSeconds.Text = "0" + Seconds;
+            }
+            else
+            {
+                labelSeconds.Text = Seconds.ToString();
+            }
+
+            if (Minutes < 10)
+            {
+                labelMinutes.Text = "0" + Minutes;
+            }
+            else
+            {
+                labelMinutes.Text = Minutes.ToString();
+            }
+
+            if (Hours < 10)
+            {
+                labelHours.Text = "0" + Hours;
+            }
+            else
+            {
+                labelHours.Text = Hours.ToString();
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            btnStart.Enabled = false;
+            btnPause.Enabled = true;
+        }
+
+        private void BtnPause_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            btnPause.Enabled = false;
+            btnStart.Enabled = true;
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            Hours = 0;
+            Minutes = 0;
+            Seconds = 0;
+
+            labelHours.Text = "00";
+            labelMinutes.Text = "00";
+            labelSeconds.Text = "00";
+        }
+    }
+}
